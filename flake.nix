ec4c474ae023890d8045e22c4832738e8ebb1488
Corey Petty <corey@status.im>
2026-03-18 09:26:35 -0400

    Fix flake: add workflow_engine moduleInput

--- flake.nix
+++ flake.nix
@@ -4,11 +4,21 @@
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
     nixpkgs.follows = "logos-module-builder/nixpkgs";
+
+    # Module dependencies (from module.yaml)
+    logos-workflow-engine = {
+      url = "github:corpetty/logos-workflow-engine";
+      inputs.logos-module-builder.follows = "logos-module-builder";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, logos-module-builder, nixpkgs }:
+  outputs = { self, logos-module-builder, nixpkgs, logos-workflow-engine }:
     logos-module-builder.lib.mkLogosModule {
       src = ./.;
       configFile = ./module.yaml;
+      moduleInputs = {
+        workflow_engine = logos-workflow-engine;
+      };
     };
 }
